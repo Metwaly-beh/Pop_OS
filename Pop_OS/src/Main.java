2a2d1d2ce555fa96aca8d203bddb152ae2032f44
Hamza Mostafa <hamzotski@gmail.com>
2026-04-16 16:32:23 +0200

    Introduce semaphores for resource management
    Added semaphores for user input, user output, and file access.

--- Pop_OS/src/Main.java
+++ Pop_OS/src/Main.java
@@ -6,7 +6,21 @@ public class Main {
     static Disk disk = new Disk();
     static Swap swap = new Swap(memory, disk);
     static List<PCB> allProcesses = new ArrayList<>();
+    public static Semaphore userInputSem = new Semaphore("userInput");
+public static Semaphore userOutputSem = new Semaphore("userOutput");
+public static Semaphore fileSem = new Semaphore("file");
 
     public static void main(String[] args) {
+        GUI gui = new GUI(this); 
          }
+
+    public static Semaphore getSemaphore(String resourceName) {
+    switch (resourceName) {
+        case "userInput": return userInputSem;
+        case "userOutput": return userOutputSem;
+        case "file": return fileSem;
+        default: return null;
+    }
+}
+    
 }
